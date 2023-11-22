c16ae1c190d43bd69c19f16c445a986f6c3a33f6
Neelachari N H <neelachari34@gmail.com>
2023-11-22 20:58:28 +0530

    movie-poster-15-done

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const connection = require("./Config/db")
 const signupRouter=require("./Routes/Signup.route")
+const LoginRouter=require("./Routes/Login.route")
 const movie=require("./Routes/movies.route")
 const cors=require("cors")
 const app=express()
@@ -10,6 +11,8 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/users",signupRouter)
+
+app.use("/users",LoginRouter)
 
 app.use("/movies",movie)
 
