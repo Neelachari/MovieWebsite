62833e93810425d07df8b202b5852c969eac9d5b
Neelachari N H <neelachari34@gmail.com>
2024-01-07 22:21:05 +0530

    watchBtn

--- Frontend/src/Components/MoviesCard.jsx
+++ Frontend/src/Components/MoviesCard.jsx
@@ -1,17 +1,19 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
 import React from 'react'
-
+import './MoviesCard.css'
 
 export const MoviesCard = ({id,Average_Rating,Description,Poster_Image,Release_Date,Runtime,Title,Trailer_URL}) => {
   return (
     <Box className='movie_Card'>
         <img width={"100%"} height={"100px"}  className="zoom-image" src={Poster_Image} alt="" />
-      {/* <Box className='details'>
-          <p style={{color:"white", textAlign:"center"}}>{Title}</p>
-          <p style={{color:"white", textAlign:"center"}}>{Release_Date}</p>
-          <p style={{color:"white", textAlign:"center"}}>{Runtime}</p>
-          <p style={{color:"white", textAlign:"center"}}>{Description}</p>
-      </Box> */}
+      <Box className='details'>
+          <h4 >{Title}</h4>
+          <p >RunTime{Runtime}</p>
+          <p >{Description}</p>
+          <Button colorScheme='blue' variant='outline' >
+    Watch Now
+  </Button>
+      </Box>
     </Box>
   )
 }
--- Frontend/src/Pages/Explore.jsx
+++ Frontend/src/Pages/Explore.jsx
@@ -1,17 +1,41 @@
 import { Box, InputGroup, InputLeftElement, Input, useColorModeValue } from '@chakra-ui/react'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { CiSearch } from "react-icons/ci"
 import { useDispatch, useSelector } from 'react-redux'
 import { getMovies } from '../Redux/MovieReducer/Action'
 import { MoviesCard } from '../Components/MoviesCard'
+import { useSearchParams } from 'react-router-dom'
 
 export const Explore = () => {
   const dispatch=useDispatch()
   const Movies=useSelector((store)=> store.productReducer.movies)
+  const [searchParms]=useSearchParams()
+  const [query,setQuery]=useState("")
+
+  // useEffect(()=>{
+  //   dispatch(getMovies())
+  // },[])
+
+  let ref=useRef()
+
+
+  //Search Results 
+  const parmobj={
+    params:{
+      q:query && query,
+
+    }
+  }
 
   useEffect(()=>{
-    dispatch(getMovies())
-  },[])
+    if(ref.current){
+      clearTimeout(ref.current)
+    }
+    ref.current=setTimeout(()=>{
+      dispatch(getMovies(parmobj))
+      },1000)
+
+  },[query])
 
 
   return (
@@ -21,7 +45,7 @@ export const Explore = () => {
     <InputLeftElement pointerEvents='none'  mt="25px" >
    <Box > <CiSearch style={{color:"white", fontSize:"30px", marginLeft:"125px"}} /></Box>
     </InputLeftElement>
-    <Input background="gray.800" w="75%" height="50px" color="white" ml="5%" type='text' placeholder='Movies, shows and more' mt="20px" />
+    <Input background="gray.800" w="75%" height="50px" color="white" ml="5%" type='text' placeholder='Movies, shows and more' mt="20px" onChange={(e)=>setQuery(e.target.value)} />
   </InputGroup>
     </Box>
     <Box className='Movies'> 
